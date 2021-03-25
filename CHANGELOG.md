5dde364a94cff40f928c2ba3b86013430e2a0dd4
samFoxfund <abadasamuelosp@gmail.com>
2021-03-25 22:57:08 +0100

    En: code fixes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,11 @@
+## 0.0.3
+
+Code fixes.
+
+## 0.0.2
+
+Updated docs.
+
 ## 0.0.1
 
 blob, tables and queue access
--- test/azstore_test.dart
+++ test/azstore_test.dart
@@ -1,10 +1,10 @@
-import 'package:azstore/constants.dart';
+// import 'package:azstore/constants.dart';
 import 'package:azstore/src/azstore_core.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:azstore/azstore.dart';
-import 'package:xml/xml.dart';
+
+import 'constants.dart';
 
 Future<void> main() async {
 //  await testDeleteQ();
@@ -25,7 +25,7 @@ Future<void> main() async {
 
 Future<void> testDeleteTableRow() async {
   try {
-    var storage = AzureStorage.parse(connectionString);
+    final storage = AzureStorage.parse(connectionString);
     await storage.deleteTableRow(tableName: 'profiles', partitionKey: 'fgtdssdas', rowKey: '232');
   }catch(e){
     print('delete exception: $e');
@@ -34,34 +34,34 @@ Future<void> testDeleteTableRow() async {
 
 Future<void> testFilterTable() async {
 
-  var storage = AzureStorage.parse(connectionString);
+  final storage = AzureStorage.parse(connectionString);
   print('working on results...');
-  List<String> result=await storage.filterTableRows(tableName: 'profiles',
+  final result=await storage.filterTableRows(tableName: 'profiles',
     filter: 'Age%20lt%2024', fields: ['Age','CustomerSince','PartitionKey','RowKey'], top: 10);
   print('showing filter results');
-  for(String res in result){
+  for(final res in result){
     print(res);
   }
 }
 
 
 Future<void> testUploadTableNode() async {
-  var storage = AzureStorage.parse(connectionString);
+  final storage = AzureStorage.parse(connectionString);
   try {
-    var myPartitionKey="sfsdfsrg57865746";
-    var myRowKey='237';
-    Map<String, dynamic> rowMap={"Address":"Santa Clara",
-      "Age":23,
-      "AmountDue":200.23,
-      "CustomerCode@odata.type":"Edm.Guid",
-      "CustomerCode":"c9da6455-213d-42c9-9a79-3e9149a57833",
-      "CustomerSince@odata.type":"Edm.DateTime",
-      "CustomerSince":"2008-07-10T00:00:00",
-      "IsActive":false,
-      "NumberOfOrders@odata.type":"Edm.Int64",
-      "NumberOfOrders":"255",
-      "PartitionKey":"$myPartitionKey",
-      "RowKey":"$myRowKey"
+    const myPartitionKey='sfsdfsrg57865746';
+    const myRowKey='237';
+    final rowMap=<String, dynamic>{'Address':'Santa Clara',
+      'Age':23,
+      'AmountDue':200.23,
+      'CustomerCode@odata.type':'Edm.Guid',
+      'CustomerCode':'c9da6455-213d-42c9-9a79-3e9149a57833',
+      'CustomerSince@odata.type':'Edm.DateTime',
+      'CustomerSince':'2008-07-10T00:00:00',
+      'IsActive':false,
+      'NumberOfOrders@odata.type':'Edm.Int64',
+      'NumberOfOrders':'255',
+      'PartitionKey':'$myPartitionKey',
+      'RowKey':'$myRowKey'
     };
   await storage.upsertTableRow(
         tableName: 'profiles',
@@ -75,11 +75,11 @@ Future<void> testUploadTableNode() async {
   }
 }
 Future<void> testGetTableRow() async {
-  var storage = AzureStorage.parse(connectionString);
+  final storage = AzureStorage.parse(connectionString);
   try {
-    var myPartitionKey="fgtdssdas";
-    var myRowKey='232';
-    String result=await storage.getTableRow(
+    const myPartitionKey='fgtdssdas';
+    const myRowKey='232';
+    final result=await storage.getTableRow(
         tableName: 'profiles',
         partitionKey:myPartitionKey,
         rowKey:myRowKey,
@@ -92,7 +92,7 @@ Future<void> testGetTableRow() async {
 }
 
 Future<void> testDeleteBlob() async {
-  var storage = AzureStorage.parse('your connection string');
+  final storage = AzureStorage.parse('your connection string');
   try {
     await storage.deleteBlob('/azpics/fdblack.png');
     print('done deleting');//Do something else
@@ -102,7 +102,7 @@ Future<void> testDeleteBlob() async {
 }
 
 Future<void> testUpdateQMessage() async {
-  var storage = AzureStorage.parse(connectionString);
+  final storage = AzureStorage.parse(connectionString);
   print('working on results...');
   try {
    await storage.updateQmessages(
@@ -117,17 +117,17 @@ Future<void> testUpdateQMessage() async {
 }
 
 Future<void> testCreateQ() async {
-  var storage = AzureStorage.parse(connectionString);
+  final storage = AzureStorage.parse(connectionString);
   await storage.createQueue('newer-queue');
 }
 
 Future<void> testGetQData() async {
-  var storage = AzureStorage.parse(connectionString);
+  final storage = AzureStorage.parse(connectionString);
   try {
-    String qName='myqueue';
-    Map<String, String> result = await storage.getQData(qName);
+    const qName='myqueue';
+    final result = await storage.getQData(qName);
     print('showing $qName data:\n');
-    for (var res in result.entries) {
+    for (final res in result.entries) {
       print('${res.key}: ${res.value}');
     }
   }catch(e){
@@ -136,23 +136,23 @@ Future<void> testGetQData() async {
 }
 
 Future<void> testDeleteQ() async {
-  var storage = AzureStorage.parse(connectionString);
+  final storage = AzureStorage.parse(connectionString);
   await storage.deleteQueue('newer-queue');
   print('done');
 }
 
 Future<void> testGetQList() async {
-  var storage = AzureStorage.parse(connectionString);
-  List<String> result=await storage.getQList();
+  final storage = AzureStorage.parse(connectionString);
+  final result=await storage.getQList();
   print('showing queue list\n');
-  for(String res in result){
+  for(final res in result){
     print(res);
   }
 }
 
 
 Future<void> testPutMessage() async {
-  var storage = AzureStorage.parse(connectionString);
+  final storage = AzureStorage.parse(connectionString);
   print('working on results...');
   try {
     await storage.putQMessage(qName:'ttable', message: 'testing expiration');
@@ -162,14 +162,14 @@ Future<void> testPutMessage() async {
 }
 
 Future<void> testGetQMessages() async {
-  var storage = AzureStorage.parse(connectionString);
+  final storage = AzureStorage.parse(connectionString);
   print('working on results...');
   try {
-    List<AzureQMessage> result = await storage.getQmessages(qName: 'ttable',//Required
+    final result = await storage.getQmessages(qName: 'ttable',//Required
       numOfmessages: 10//Optional. Number of messages to retrieve. This package returns top 20 filter results when not specified.
     );
     print('showing results');
-    for (var res in result) {
+    for (final res in result) {
       print('message ${res}');
     }
   }catch (e){
@@ -178,11 +178,11 @@ Future<void> testGetQMessages() async {
 }
 
 Future<void> testPeekQMessages() async {
-  var storage = AzureStorage.parse(connectionString);
+  final storage = AzureStorage.parse(connectionString);
   try {
-    List<AzureQMessage> result = await storage.peekQmessages(qName: 'ttable');
+    final result = await storage.peekQmessages(qName: 'ttable');
     print('showing peek results');
-    for (var res in result) {
+    for (final res in result) {
       print('message ${res.messageText}');
     }
   }catch (e){
@@ -191,7 +191,7 @@ Future<void> testPeekQMessages() async {
 }
 
 Future<void> testClearQMessage() async {
-  var storage = AzureStorage.parse(connectionString);
+  final storage = AzureStorage.parse(connectionString);
   print('working on results...');
   try {
     await storage.clearQmessages('ttable');
@@ -202,7 +202,7 @@ Future<void> testClearQMessage() async {
 }
 
 Future<void> testDeleteMessage() async {
-  var storage = AzureStorage.parse(connectionString);
+  final storage = AzureStorage.parse(connectionString);
   print('working on results...');
   try {
     await storage.delQmessages(qName: 'ttable', messageId: '27bc633b-4de0-42bf-bea6-0860bd410f4e', popReceipt: 'AgAAAAMAAAAAAAAAX3e0UwAg1wE=');
